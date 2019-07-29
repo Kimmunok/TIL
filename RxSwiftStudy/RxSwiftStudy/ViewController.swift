16a83c1548c049084188bdf4f9b2c017459aad99
Munok <wer0222@naver.com>
2019-07-29 22:47:40 +0900

    2주차 실습

--- RxSwiftStudy/RxSwiftStudy/ViewController.swift
+++ RxSwiftStudy/RxSwiftStudy/ViewController.swift
@@ -17,7 +17,9 @@ class ViewController: UIViewController {
     
     let viewModel = SigninViewModel()
     
-//    var tv = UITableView()
+    var tv = UITableView()
+    let tvData: [String] = ["Seoul", "New York", "London", "Tokyo", "Paris", "Moscow", "Beijing"]
+    let reuseId = "MyCell"
 //    let list = BehaviorRelay<[String]>(value: ["1", "2", "3", "4", "5"])
     var emailTextField = UITextField()
     var passwordTextField = UITextField()
@@ -43,6 +45,7 @@ class ViewController: UIViewController {
         
         configueUI()
         bindViewModel()
+        configueTableView()
     }
     
     func configueUI() {
@@ -132,5 +135,99 @@ class ViewController: UIViewController {
             .bind(to: signinButton.rx.isEnabled)
             .disposed(by: disposeBag)
     }
+    
+    
+    func configueTableView() {
+        self.view.addSubview(tv)
+        
+        tv.snp.makeConstraints { (m) in
+            m.leading.trailing.bottom.equalToSuperview()
+            m.top.equalTo(signinButton.snp.bottom).offset(50)
+        }
+        
+        tv.register(MyCell.self, forCellReuseIdentifier: reuseId)
+        
+        /*
+         Bind the data to the tableView/collectionView using one of:
+         
+         1. rx.items(dataSource:protocol<RxTableViewDataSourceType, UITableViewDataSource>)
+         2. rx.items(cellIdentifier:String)
+         3. rx.items(cellIdentifier:String:Cell.Type:_:)
+         4. rx.items(_:_:)
+         */
+        
+        let dataOb: Observable<[String]> = Observable.of(tvData)
+        
+        // 1. tableView.rx.items 사용하기
+//        dataOb
+//            .bind(to: tv.rx.items) { (tv: UITableView, index: Int, element: String) -> UITableViewCell in
+//                guard let cell = tv.dequeueReusableCell(withIdentifier: self.reuseId) else { return UITableViewCell() }
+//                cell.textLabel?.text = element
+//                return cell
+//        }.disposed(by: disposeBag)
+        
+        // 2. tableView.rx.items(cellIdentifier:String) 사용하기
+//        dataOb
+//            .bind(to: tv.rx.items(cellIdentifier: reuseId)) { (index: Int, element: String, cell: UITableViewCell) in
+//                cell.textLabel?.text = element
+//        }.disposed(by: disposeBag)
+        
+        // 3. tableView.rx.items(cellIdentifier: String, cellType: Cell.Type) 사용하기
+//        dataOb
+//            .bind(to: tv.rx.items(cellIdentifier: reuseId, cellType: MyCell.self)) { (index: Int, element: String, cell: MyCell) in
+//                cell.myLabel.text = "\(index) : \(element)"
+//        }.disposed(by: disposeBag)
+        
+        // 4. tableView.rx.items(dataSource:protocol<RxTableViewDataSourceType, UITableViewDataSource>) 사용하기
+        
+        // 섹션 및 섹션타이틀 지정
+        let sections = [
+            SectionModel<String, String>(model: "First Section", items: tvData),
+            SectionModel<String, String>(model: "Second Section", items: tvData)
+        ]
+
+        // 섹션으로 스트림 시작
+        Observable.just(sections)
+            .bind(to: tv.rx.items(dataSource: myDataSource))
+            .disposed(by: disposeBag)
+    }
+
+    // 이름이 너무 긴 것을 Typealias를 통해 줄여줌
+    typealias TvDataSectionModel = SectionModel<String, String>
+    typealias TvDataDataSource = RxTableViewSectionedReloadDataSource<TvDataSectionModel>
+
+    // datasource 만드는 부분을 프로퍼티로 빼줌
+    private var myDataSource: TvDataDataSource {
+        // SectionModel로 DataSource를 생성할 때 인자로 집어넣을 구성된 셀
+        let configureCell: (TableViewSectionedDataSource<TvDataSectionModel>, UITableView, IndexPath, String) -> UITableViewCell = {
+            (datasource, tableView, indexPath, element) in
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: self.reuseId, for: indexPath) as? MyCell else { return UITableViewCell() }
+            cell.myLabel.text = element
+            cell.textLabel?.text = "\(indexPath.item) : \(element)"
+            return cell
+        }
+
+        // RxTableViewSectionedReloadDataSource 생성
+        let dataSource = TvDataDataSource.init(configureCell: configureCell)
+
+        // 섹션타이틀 지정
+        dataSource.titleForHeaderInSection = { dataSource, index in
+            return dataSource.sectionModels[index].model
+        }
+        
+        return dataSource
+    }
 }
 
+class MyCell: UITableViewCell {
+    
+    var myLabel = UILabel()
+    
+    override func draw(_ rect: CGRect) {
+        myLabel.frame = self.bounds
+        myLabel.textColor = .blue
+        myLabel.backgroundColor = UIColor(white: 1.0, alpha: 0.0)
+        myLabel.textAlignment = .center
+        self.addSubview(myLabel)
+    }
+}
